b63aa2551fe1fa3bcf2d8d66d1412a5deb9e2b54
CHELSA ANNA SELVY <chelsaannaselvy@gmail.com>
2024-09-04 19:57:34 +0530

    Second Commit

---
+++ js_assignment_2.js
@@ -0,0 +1,15 @@
+let n = 5
+function is_even(n)
+{
+    if(n % 2 == 0)
+    {
+        return "Even"
+    }
+    else
+    {
+        return "Odd"
+    }
+}
+
+let result = is_even(n)
+console.write(result)
